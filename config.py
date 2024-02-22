6972a1b27a44bb364f6580f3962a84c4a87b3fef
DewasSquid <yohansambin@gmail.com>
2024-02-21 21:41:50 -0300

    switching order of commands

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ WINDOW_TITLE = "Doxxing Caterpillar"
 VIDEO_PATH = "./assets/video.mp4"
 MUSIC_PATH = "./assets/audio.mp3"
 MUSIC_VOLUME = .1
-TEXT_COMMANDS = ["ipconfig", "systeminfo", "netsh wlan show profiles", "tasklist"]
+TEXT_COMMANDS = ["ipconfig", "tasklist", "systeminfo", "netsh wlan show profiles"]
 TEXT_INTERVAL = 15
 TEXT_STEPS = 10
