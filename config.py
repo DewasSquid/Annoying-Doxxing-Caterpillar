2b98eb1bc1610b4cff771a0aaee1a7fb02dfdc87
DewasSquid <yohansambin@gmail.com>
2024-02-22 15:23:07 -0300

    added pyinstaller support for assets

--- config.py
+++ config.py
@@ -1,9 +1,18 @@
+import sys
+import os
+
+def resource_path(relative_path):
+    """Support for pyinstaller"""
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
 # Title for the main window.
 WINDOW_TITLE = "Doxxing Caterpillar"
 # Path to the video file for playback.
-VIDEO_PATH = "./assets/video.mp4"
+VIDEO_PATH = resource_path("assets/video.mp4")
 # Path to the audio file for background music.
-MUSIC_PATH = "./assets/audio.mp3"
+MUSIC_PATH = resource_path("assets/audio.mp3")
 # Volume level for the background music (0.0 to 1.0).
 MUSIC_VOLUME = .1
 # List of system commands to retrieve information for display.
